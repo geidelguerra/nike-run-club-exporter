16d81ab3e24927227dc0f21dfcc2a04085b953ff
Geidel Guerra <geidelguerra@protonmail.com>
2022-05-23 20:34:35 -0400

    Clean API and update readme

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ const api = new NikeRunClub(settings)
 program.version('1.0.0');
 
 program
-  .command('token')
+  .command('set-token')
   .description('Set token')
   .action(async () => {
     const input = await enquirer.prompt([
@@ -38,62 +38,10 @@ program
   });
 
 program
-  .command('credentials')
-  .description('Set login credentials')
-  .action(async () => {
-    const credentials = await enquirer.prompt([
-      {
-        type: 'input',
-        name: 'email',
-        message: 'Email',
-        validate: (value) => {
-          if (value === '') {
-            return 'Email required'
-          }
-
-          return true
-        }
-      },
-      {
-        type: 'password',
-        name: 'password',
-        message: 'Password',
-        validate: (value) => {
-          if (value === '') {
-            return 'Password required'
-          }
-
-          return true
-        }
-      }
-    ])
-
-    settings.set({ credentials })
-  });
-
-
-program
-  .command('manual-login')
-  .description('Manual login')
+  .command('open-login-page')
+  .description('Open login page')
   .action(async () => {
     await api.manualLogin()
-  });
-
-program
-  .command('login')
-  .description('Automatic login')
-  .action(async () => {
-    await api.login()
-  });
-
-program
-  .command('activities')
-  .argument('[afterId]')
-  .description('List activities')
-  .action(async (afterId) => {
-    const response = await api.activities({ afterId })
-
-    console.log(response)
   });
 
 program
